50c312f875272894965aad0a16f32fd065fa1f77
cryptid <cryptid@astranetwork.com>
2022-10-23 17:08:23 -0400

    mayc

--- src/pages/mayc.tsx
+++ src/pages/mayc.tsx
@@ -3,10 +3,17 @@ import Layout from "../components/Layout";
 import { NextPage } from "next";
 import mayc from "../files/mayc.json";
 import Button from "../components/Button";
+import { FormEventHandler, useState } from "react";
 
-const alphabet = "ABCDEFG";
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const MAYC: NextPage = () => {
+  const [entryStatus, setEntryStatus] = useState<boolean | null>(null);
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    setEntryStatus(false);
+  };
+
   return (
     <Layout>
       <Heading>MAYC Giveaway</Heading>
@@ -20,7 +27,7 @@ const MAYC: NextPage = () => {
             </div>
           ))}
         </div>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="space-y-8">
             <p>Check entry status</p>
             <div className="flex items-center">
@@ -29,6 +36,7 @@ const MAYC: NextPage = () => {
             </div>
             <Button>Check</Button>
           </div>
+          {entryStatus === false && <div>Not accepted yet</div>}
         </form>
         <Heading className="text-yellow">{Object.keys(mayc)[1]}</Heading>
         <div className="text-xl space-y-4">
