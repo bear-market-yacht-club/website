3a66651d333beb7cbb505003f9446ebaaf27ac7e
cryptid <cryptid@astranetwork.com>
2022-10-25 12:58:01 -0400

    updated mint page

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -124,91 +124,93 @@ const Mint: NextPage = () => {
             <></>
           )}
         </div>
-        <div className="flex flex-col items-center gap-4">
-          <div>
-            <Checkbox
-              checked={agreed}
-              onChange={() => setAgreed((prev) => !prev)}
-              label={
-                <span>
-                  Check to agree to the{" "}
-                  <Link href="/tos.txt">Terms of Service</Link> and{" "}
-                  <Link href="/privacy-policy.txt">Privacy Policy</Link>
-                </span>
-              }
-            />
-          </div>
-          {state.status === "None" ? (
+        {mintTime && (
+          <div className="flex flex-col items-center gap-4">
             <div>
-              <div className="flex gap-8">
-                <div className="flex items-stretch text-yellow">
-                  <input
-                    className="bg-black border-b-yellow border-b-2 w-12 text-3xl text-center"
-                    defaultValue={0}
-                    min={1}
-                    max={10}
-                    value={quantity}
-                    type="number"
-                    onChange={onQuantityChange}
-                  />
-                  <div className="flex flex-col w-6">
-                    <svg
-                      className="flex-grow cursor-pointer"
-                      onClick={() =>
-                        setQuantity((prev) => Math.min(10, prev + 1))
-                      }
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                    >
-                      <path
-                        fill="#bfc500"
-                        d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
-                      />
-                    </svg>
-                    <svg
-                      className="flex-grow cursor-pointer"
-                      onClick={() =>
-                        setQuantity((prev) => Math.max(0, prev - 1))
-                      }
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path
-                        fill="#bfc500"
-                        d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
-                      />
-                    </svg>
-                  </div>
-                </div>
-                <Button
-                  onClick={onMint}
-                  disabled={!agreed || whitelisted !== true || !mintTime}
-                >
-                  Mint
-                </Button>
-              </div>
-              {!mintTime ? (
-                <div className="mt-4 text-red-500">Not minting time yet</div>
-              ) : !account ? (
-                <div className="mt-4 text-red-500">Connect wallet to mint</div>
-              ) : (
-                !agreed && (
-                  <div className="mt-4 text-red-500">
-                    You must agree to the Terms of Service and Privacy Policy to
-                    mint
-                  </div>
-                )
-              )}
+              <Checkbox
+                checked={agreed}
+                onChange={() => setAgreed((prev) => !prev)}
+                label={
+                  <span>
+                    Check to agree to the{" "}
+                    <Link href="/tos.txt">Terms of Service</Link> and{" "}
+                    <Link href="/privacy-policy.txt">Privacy Policy</Link>
+                  </span>
+                }
+              />
             </div>
-          ) : state.status === "Mining" ||
-            state.status === "PendingSignature" ? (
-            <div className="text-yellow">Confirming Transaction</div>
-          ) : state.status === "Success" ? (
-            <div className="text-green-400">Success!</div>
-          ) : (
-            <div className="text-red-500">Unknown error occured</div>
-          )}
-        </div>
+            {state.status === "None" ? (
+              <div>
+                <div className="flex gap-8">
+                  <div className="flex items-stretch text-yellow">
+                    <input
+                      className="bg-black border-b-yellow border-b-2 w-12 text-3xl text-center"
+                      defaultValue={0}
+                      min={1}
+                      max={10}
+                      value={quantity}
+                      type="number"
+                      onChange={onQuantityChange}
+                    />
+                    <div className="flex flex-col w-6">
+                      <svg
+                        className="flex-grow cursor-pointer"
+                        onClick={() =>
+                          setQuantity((prev) => Math.min(10, prev + 1))
+                        }
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="#bfc500"
+                          d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
+                        />
+                      </svg>
+                      <svg
+                        className="flex-grow cursor-pointer"
+                        onClick={() =>
+                          setQuantity((prev) => Math.max(0, prev - 1))
+                        }
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                      >
+                        <path
+                          fill="#bfc500"
+                          d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <Button
+                    onClick={onMint}
+                    disabled={!agreed || whitelisted !== true || !mintTime}
+                  >
+                    Mint
+                  </Button>
+                </div>
+                {!account ? (
+                  <div className="mt-4 text-red-500">
+                    Connect wallet to mint
+                  </div>
+                ) : (
+                  !agreed && (
+                    <div className="mt-4 text-red-500">
+                      You must agree to the Terms of Service and Privacy Policy
+                      to mint
+                    </div>
+                  )
+                )}
+              </div>
+            ) : state.status === "Mining" ||
+              state.status === "PendingSignature" ? (
+              <div className="text-yellow">Confirming Transaction</div>
+            ) : state.status === "Success" ? (
+              <div className="text-green-400">Success!</div>
+            ) : (
+              <div className="text-red-500">Unknown error occured</div>
+            )}
+          </div>
+        )}
       </div>
     </Layout>
   );
