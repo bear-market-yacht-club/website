cefa043f17d90ce10d00c99208335f7aea5cd323
cryptid <cryptid@astranetwork.com>
2022-10-27 12:09:05 -0400

    removed whitelist

--- src/server/router/mint.ts
+++ src/server/router/mint.ts
@@ -6,22 +6,6 @@ import { createRouter } from "./context";
 export const mint = createRouter().query("merkle-proof", {
   input: addressValidator,
   async resolve({ input, ctx }) {
-    const whitelistedAddresses = await ctx.prisma.whitelists.findMany();
-    if (whitelistedAddresses) {
-      const leafNodes = whitelistedAddresses.map((addr) =>
-        keccak256(addr.address.toLowerCase())
-      );
-      const merkleTree = new MerkleTree(leafNodes, keccak256, {
-        sortPairs: true,
-      });
-
-      const merkleProof = merkleTree.getHexProof(keccak256(input.ethAddress));
-      const whitelisted = merkleTree.verify(
-        merkleProof,
-        keccak256(input.ethAddress),
-        merkleTree.getHexRoot()
-      );
-      return { merkleProof, whitelisted };
-    }
+    return { merkleProof: [], whitelisted: false };
   },
 });
