9a03d1fa228a5ba7e4dfe2279af3800f73bd9d1a
cryptid <cryptid@astranetwork.com>
2022-10-28 11:48:44 -0400

    paused mint

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,81 +1,20 @@
-import { Mainnet, useEthers, useEtherBalance } from "@usedapp/core";
-import { compareAsc, intervalToDuration } from "date-fns";
-import { utils } from "ethers";
 import type { NextPage } from "next";
 import Image from "next/image";
-import Link from "next/link";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../components/Button";
-import Checkbox from "../components/Checkbox";
 import Heading from "../components/Heading";
 import Layout from "../components/Layout";
-import { useIsWhitelistUsed, useMint, useTotalSupply } from "../hooks";
 import { trpc } from "../utils/trpc";
-import { Modal } from "react-responsive-modal";
-import { log } from "next-axiom";
-import { useRouter } from "next/router";
-
-const TimeSlot = ({ unit, amount }: { unit: string; amount?: number }) => {
-  return (
-    <div className="text-center min-w-20">
-      <div className="font-black text-8xl md:text-6xl">{amount}</div>
-      <div className="uppercase text-2xl md:text-lg">{unit}</div>
-    </div>
-  );
-};
 
 const Mint: NextPage = () => {
-  const router = useRouter();
-  const { account, chainId } = useEthers();
-  const etherBalance = useEtherBalance(account);
-  const [agreed, setAgreed] = useState(false);
-  const [mintDuration, setMintDuration] = useState<Duration>();
-  const [quantity, setQuantity] = useState(3);
-  const { send, state } = useMint();
-  const { data: mintData } = trpc.useQuery([
-    "mint.merkle-proof",
-    { ethAddress: account || "0x" },
-  ]);
-  const [mintTime, setMintTime] = useState(false);
-  const isWhitelistUsed = useIsWhitelistUsed(account);
-  const totalSupply = useTotalSupply();
   const { mutate: addEmail, status: emailStatus } =
     trpc.useMutation("mailing.addEmail");
   const emailRef = useRef<HTMLInputElement>(null);
   const [twitterHandle, setTwitterHandle] = useState<string | undefined>(
     undefined
   );
-  let merkleProof: string[],
-    whitelisted = false;
-  if (mintData) {
-    ({ merkleProof, whitelisted } = mintData);
-  }
-
-  const mintCost = utils.parseEther(
-    (0.06 * (quantity - (whitelisted && !isWhitelistUsed ? 1 : 0))).toString()
-  );
 
   useEffect(() => {
-    log.debug(JSON.stringify({ account, whitelisted }));
-  }, [account]);
-
-  useEffect(() => {
-    const setDuration = () => {
-      const nowUTC = new Date();
-      const mintTimeUTC = new Date("2022-10-26T20:20:00Z");
-      const d = intervalToDuration({
-        start: nowUTC,
-        end: mintTimeUTC,
-      });
-
-      setMintDuration(d);
-      setMintTime(compareAsc(mintTimeUTC, nowUTC) === -1);
-    };
-    setDuration();
-    const intervalId = setInterval(() => {
-      setDuration();
-    }, 1000);
-
     const th = localStorage
       .getItem("twitter_handle")
       ?.toLowerCase()
@@ -86,28 +25,7 @@ const Mint: NextPage = () => {
       localStorage.setItem("twitter_handle", th);
       setTwitterHandle(th);
     }
-
-    return () => clearInterval(intervalId);
   }, []);
-
-  useEffect(() => {
-    log.debug(JSON.stringify(state));
-  }, [state.status]);
-
-  const onMint = async () => {
-    const tx = await send(quantity, merkleProof, {
-      value: mintCost,
-      gasLimit: 200000,
-    });
-    console.log(tx);
-  };
-
-  const onQuantityChange = (value: ChangeEvent<HTMLInputElement>) => {
-    let newQuantity = parseInt(value.currentTarget.value) || 0;
-    newQuantity = Math.max(1, newQuantity);
-    newQuantity = Math.min(10, newQuantity);
-    setQuantity(newQuantity);
-  };
 
   const subscribe = () => {
     if (emailRef.current?.value) {
@@ -131,193 +49,25 @@ const Mint: NextPage = () => {
           />
         </div>
         <Heading>
-          {!mintTime ? (
-            "Minting:"
-          ) : (
-            <span className="text-yellow">Minting Live!</span>
-          )}
+          <span className="text-yellow">Minting Paused</span>
         </Heading>
-        {!mintTime ? (
-          <div className="-mt-12 md:-mt-8 scale-50 gap-12 md:scale-100 flex">
-            <TimeSlot unit="days" amount={mintDuration?.days} />
-            <TimeSlot unit="hours" amount={mintDuration?.hours} />
-            <TimeSlot unit="minutes" amount={mintDuration?.minutes} />
-            <TimeSlot unit="seconds" amount={mintDuration?.seconds} />
-          </div>
-        ) : (
-          <div className="-mt-12 md:-mt-8 scale-50 gap-12 md:scale-100 flex">
-            <div className="font-black text-8xl md:text-5xl">
-              {totalSupply ?? 0} / 5555
-            </div>
-          </div>
-        )}
-        <div className="md:mt-4 flex flex-col gap-4 justify-between items-center text-center">
-          {whitelisted === null ? (
-            <p>
-              Check if you&apos;re whitelisted by connecting your MetaMask
-              wallet
-            </p>
-          ) : whitelisted === true && !isWhitelistUsed ? (
-            <p>You have been whitelisted!</p>
-          ) : isWhitelistUsed ? (
-            <p>You have already claimed your free mint</p>
-          ) : (
-            <></>
-          )}
+        <div>Enter your email to get notified of any updates</div>
+        <div className="flex gap-4 text-xl">
+          <input
+            className=""
+            ref={emailRef}
+            placeholder="email@example.com"
+            type="text"
+          />
+          <Button
+            className="p-1 bg-transparent text-yellow border-2 !border-solid border-yellow"
+            onClick={subscribe}
+          >
+            Submit
+          </Button>
         </div>
-        {mintTime && (
-          <div className="flex flex-col items-center gap-4">
-            <div>
-              <Checkbox
-                checked={agreed}
-                onChange={() => setAgreed((prev) => !prev)}
-                label={
-                  <span>
-                    Check to agree to the{" "}
-                    <Link href="/tos.txt">Terms of Service</Link> and{" "}
-                    <Link href="/privacy-policy.txt">Privacy Policy</Link>
-                  </span>
-                }
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <div className="flex gap-8">
-                <div className="flex items-stretch text-yellow">
-                  <input
-                    className="bg-black border-b-yellow border-b-2 w-12 text-3xl text-center"
-                    defaultValue={10}
-                    min={1}
-                    max={10}
-                    value={quantity}
-                    type="number"
-                    onChange={onQuantityChange}
-                  />
-                  <div className="flex flex-col w-6">
-                    <svg
-                      className="flex-grow cursor-pointer"
-                      onClick={() =>
-                        setQuantity((prev) => Math.min(10, prev + 1))
-                      }
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                    >
-                      <path
-                        fill="#bfc500"
-                        d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
-                      />
-                    </svg>
-                    <svg
-                      className="flex-grow cursor-pointer"
-                      onClick={() =>
-                        setQuantity((prev) => Math.max(1, prev - 1))
-                      }
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path
-                        fill="#bfc500"
-                        d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
-                      />
-                    </svg>
-                  </div>
-                </div>
-                <Button
-                  className=""
-                  onClick={onMint}
-                  disabled={!agreed || !mintTime || etherBalance?.lt(mintCost)}
-                >
-                  Mint
-                </Button>
-              </div>
-              {!account ? (
-                <div className="mt-4 text-red-500">Connect wallet to mint</div>
-              ) : chainId !== Mainnet.chainId ? (
-                <div className="mt-4 text-red-500">
-                  Change network to Ethereum mainnet to mint
-                </div>
-              ) : !agreed ? (
-                <div className="mt-4 text-red-500">
-                  You must agree to the Terms of Service and Privacy Policy to
-                  mint
-                </div>
-              ) : (
-                etherBalance?.lt(mintCost) && (
-                  <div className="mt-4 text-red-500">
-                    Not enough ether. Mint price is 0.06 eth per bear
-                  </div>
-                )
-              )}
-            </div>
-            {state.status === "PendingSignature" ||
-            state.status === "Mining" ? (
-              <div className="text-yellow">Confirming Transaction</div>
-            ) : state.status === "Fail" ? (
-              <div className="text-red-500">
-                {state.errorCode}: {state.errorMessage}
-              </div>
-            ) : (
-              <div></div>
-            )}
-          </div>
-        )}
-        <Modal
-          open={state.status === "Success"}
-          onClose={() => {
-            router.reload();
-          }}
-          center
-          styles={{
-            modal: {
-              backgroundColor: "black",
-              border: "2px solid #bfc500",
-              borderRadius: "1em",
-            },
-            closeButton: {
-              top: "1.5em",
-              right: "1.5em",
-              fill: "#bfc500",
-            },
-          }}
-        >
-          <div className="flex flex-col items-center">
-            <div className="text-green-400 text-3xl mb-2">Success!</div>
-            <div className="text-center">
-              Welcome to our big happy bear fam!
-              <br />
-              Stay tuned for the art reveal!
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <a
-                className="my-4 no-underline"
-                rel="noreferrer"
-                target="_blank"
-                href={`https://twitter.com/intent/tweet?text=I just minted ${quantity} bear${
-                  quantity > 1 ? "s" : ""
-                }!! Mint is live @ bmyc.io/mint! 🐻 @BearMarketYC`}
-              >
-                <Button>Tweet</Button>
-              </a>
-              <hr className="h-2 w-full text-white" />
-              <div>Enter your email to subscribe to our club list</div>
-              <div className="flex gap-4 text-xl">
-                <input
-                  className=""
-                  ref={emailRef}
-                  placeholder="email@example.com"
-                  type="text"
-                />
-                <Button
-                  className="p-1 bg-transparent text-yellow border-2 !border-solid border-yellow"
-                  onClick={subscribe}
-                >
-                  Submit
-                </Button>
-              </div>
-              {emailStatus === "error" && <div>Invalid Email</div>}
-              {emailStatus === "success" && <div>Submitted</div>}
-            </div>
-          </div>
-        </Modal>
+        {emailStatus === "error" && <div>Invalid Email</div>}
+        {emailStatus === "success" && <div>Submitted</div>}
       </div>
     </Layout>
   );
